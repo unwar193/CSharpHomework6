1721453bf47b5d80565870644653eaed2fd0c18f
unwar193 <varan193@mail.ru>
2022-10-08 19:32:25 +0300

    homework6

---
+++ Homework6.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры число M, потом вводит M чисел ЧЕРЕЗ ENTER. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/* 5
+0
+7
+8
+-2
+-2 -> 2
+
+5
+1
+-7
+567
+89
+223-> 3 */
+
+Console.Clear();
+Console.WriteLine("Введите целое положительное число M: ");
+int countM = int.Parse(Console.ReadLine()!);
+int countPos = 0;
+
+for (int i = 0; i < countM; i++)
+{
+    Console.WriteLine($"Введите любое целое число ещё {countM - i} раз:  ");
+    int countTemp = int.Parse(Console.ReadLine()!);
+    if (countTemp > 0){
+        countPos++;
+    };
+    /* Console.WriteLine($"{countTemp}"); */
+}
+
+
+ Console.WriteLine($"Чисел больше нуля введено: {countPos}");
---
+++ Homework6.2/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+Console.Clear();
+Console.WriteLine("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+
+Console.Write($"Координаты точки пересечения: {x}; {y}"); 
